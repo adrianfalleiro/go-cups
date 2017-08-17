6c0f65426ff7449a509f5096de2d54d30dc93e16
Adrian Falleiro <adrian.falleiro@analogfolk.com>
2017-08-17 16:54:37 +1000

    Attempted wrapper function

--- main.go
+++ main.go
@@ -6,17 +6,32 @@ package main
 #include <stdio.h>
 #include <stdlib.h>
 #include "cups/cups.h"
+void print_dest(void *user_data, unsigned flags, cups_dest_t *dest)
+{
+  	if (dest->instance)
+    	printf("%s/%s\n", dest->name, dest->instance);
+	else
+		printf("a%s\n", dest->name);
+}
+int listPrinters()
+{
+	cups_ptype_t mask = CUPS_PRINTER_LOCAL;
+	cups_ptype_t type = CUPS_PRINTER_LOCAL;
+	int msec = 0;
+	int cancel;
+
+  	return cupsEnumDests(CUPS_DEST_FLAGS_NONE, msec, NULL, type, mask, (cups_dest_cb_t)print_dest, NULL);
+}
 */
 import "C"
 
-import "fmt"
-
 func main() {
 
-	var dests *C.cups_dest_t
-	numDests := C.cupsGetDests(&dests)
-	defaultDest := C.cupsGetDest(nil, nil, numDests, dests)
-	defaultDestName := C.GoString(defaultDest.name)
-	fmt.Print(defaultDestName)
+	ListPrinters()
 
+}
+
+// ListPrinters function ...
+func ListPrinters() {
+	C.listPrinters()
 }
